c18e1150fc720cd19a3bae7a2ecfc2132de3b810
문진우 <mjw7169@kakao.com>
2026-09-25 23:14:44 +0900

    fix: configure Vite for project Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/wargame/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'app.html',
+    },
+  },
 })
