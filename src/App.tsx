53b7b8e0e5655af871ef7321aefdaffe40e91d34
문진우 <mjw7169@kakao.com>
2026-09-25 23:06:16 +0900

    fix: update MapLibre module import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
-import maplibregl from 'maplibre-gl'
+import * as maplibregl from 'maplibre-gl'
 import { loadLatestAdminDongs } from './adminData'
 import {
   advanceTick,
